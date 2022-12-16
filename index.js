39d4b5eb3af71063a03c663328fc0b7c0a762f58
Joao <jonhlhn.surf@gmail.com>
2022-12-16 15:33:57 -0300

    Create an operate function

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      add(a, b);
+      break;
+    case "-":
+      subtract(a, b);
+      break;
+    case "*":
+      multiply(a, b);
+      break;
+    case "/":
+      divide(a, b);
+      break;
+    default:
+      console.error(`Invalid operator ${operator}`);
+  }
+}

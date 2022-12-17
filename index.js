299cb84cbc5701f97641b0d1b9b479e6b27ff185
Joao <jonhlhn.surf@gmail.com>
2022-12-17 10:46:31 -0300

    Reorganize markup & add number with listeners

--- index.js
+++ index.js
@@ -2,35 +2,18 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
 let displayValue = "";
-function populateDisplay() {
-  addEventListener("keyup", (e) => {
-    if (
-      !isNaN(e.key) ||
-      e.key === "+" ||
-      e.key === "-" ||
-      e.key === "*" ||
-      e.key === "/"
-    ) {
-      displayValue += e.key;
-    }
-  });
+const display = document.querySelector("#display");
+
+function operate(a, operator, b) {
+  return;
 }
-function operate(operator, a, b) {
-  switch (operator) {
-    case "+":
-      add(a, b);
-      break;
-    case "-":
-      subtract(a, b);
-      break;
-    case "*":
-      multiply(a, b);
-      break;
-    case "/":
-      divide(a, b);
-      break;
-    default:
-      console.error(`Invalid operator ${operator}`);
-  }
-}
+
+const numberKeys = document.querySelectorAll(".number");
+
+[...numberKeys].forEach((number) =>
+  number.addEventListener("click", (e) => populateDisplay(e.target.textContent))
+);
+
+

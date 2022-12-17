53b7f9d1ade711e02bcb3ab99349304b2503fe6b
Joao <jonhlhn.surf@gmail.com>
2022-12-17 15:35:51 -0300

    Define  referencing  when dividing by 0

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+function operate(a, operator, b) {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    case "multiply":
+      return multiply(a, b);
+    case "divide":
+      return b === 0 ? displayError() : divide(a, b);
+    default:
+      console.warn(`Invalid operator ${operator}`);
+  }
+}

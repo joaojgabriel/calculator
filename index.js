26d57e322df22457ec165bb4a5ee7de2c53af816
Joao <jonhlhn.surf@gmail.com>
2022-12-18 11:46:41 -0300

    Create operate function

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
+function operate(a, operator, b) {
+  switch (operator) {
+    case "add":
+      return add(a, b);
+    case "subtract":
+      return subtract(a, b);
+    case "multiply":
+      return multiply(a, b);
+    case "divide":
+      return divide(a, b);
+  }
+}
